5a84648434d637805e6b39be1c3aa88dfdac5792
Zhikai Ye <zhikaiye939@gmail.com>
2019-08-27 20:00:20 +0800

    push

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase {
+
+
+
+
+    @Test
+    public void case01(){
+        System.out.println("aaa");
+    }
+
+    @Test
+    public void case02(){
+        Assert.assertEquals("a","b");
+    }
+}
